056c4e721ff491af5319864a1f053580d719a3f5
Ujjwal Gupta <ujjwalkumargupta4u@gmail.com>
2019-02-09 11:43:22 +0530

    added store

---
+++ src/store/store.ts
@@ -0,0 +1,13 @@
+import Vuex from "vuex";
+import { states } from "./states";
+import { mutations } from "./mutations";
+import Vue from "vue";
+import { actions } from "./actions";
+import { StateOption } from "../types/state_option";
+Vue.use(Vuex);
+
+export const store = new Vuex.Store<StateOption>({
+    state: states,
+    mutations: mutations,
+    actions: actions
+});
